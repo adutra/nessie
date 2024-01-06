5a62cbf37f041b32a48cde7c0cf40993ec20cfac
Robert Stupp <snazy@snazy.de>
2024-01-06 10:08:41 +0100

    Catalog: remove initial inmemory catalog storage

--- bom/build.gradle.kts
+++ bom/build.gradle.kts
@@ -98,7 +98,6 @@ dependencies {
     api(project(":nessie-catalog-service-server-combined"))
     api(project(":nessie-catalog-service-server-tests"))
     api(project(":nessie-catalog-storage-backend"))
-    api(project(":nessie-catalog-storage-inmemory"))
     api(project(":nessie-catalog-storage-persist"))
     api(project(":nessie-catalog-iceberg-catalog"))
     api(project(":nessie-catalog-iceberg-httpfileio"))
